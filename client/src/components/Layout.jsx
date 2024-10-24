68dab68f615ee4830d1a776dd7bf6e8b15156ee4
Chokkalingam.s <chok22017.it@rmkec.ac.in>
2024-10-24 16:32:35 +0530

    Add ward - Layout

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -3,21 +3,23 @@ import {
   Routes,
   Route,
   useLocation,
-} from 'react-router-dom'
-import AuthComponent from './AuthComp'
-import Dashboard from './Dashboard'
-import Navbar from './Navbar'
-import Awareness from './Awareness'
-import Progress from './Progress'
-import NFCPage from './NFC'
-import states from '../data/stateAndDistrict.json'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+} from 'react-router-dom';
+import AuthComponent from './AuthComp';
+import Dashboard from './Dashboard';
+import Navbar from './Navbar';
+import Awareness from './Awareness';
+import Progress from './Progress';
+import NFCPage from './NFC';
+import Profile from './Profile';
+import states from '../data/stateAndDistrict.json';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import NFCCard from './NFC-Card'
 
 function LayoutContent({ data, credits }) {
-  const location = useLocation()
-  const districts = ['District 1', 'District 2']
-  const wards = ['Ward 1', 'Ward 2']
+  const location = useLocation();
+  const districts = ['District 1', 'District 2'];
+  const wards = ['Choose Your Ward','Ward 1', 'Ward 2'];
 
   return (
     <div
@@ -30,32 +32,16 @@ function LayoutContent({ data, credits }) {
       <main>
         <Routes>
           <Route
-            path='/dashboard'
-            element={
-              <Dashboard
-                BarGraphVals={data}
-                creditVal={credits}
-              />
-            }
+            path="/dashboard"
+            element={<Dashboard BarGraphVals={data} creditVal={credits} />}
           />
+          <Route path="/awareness" element={<Awareness />} />
+          <Route path="/progress" element={<Progress />} />
+          <Route path="/nfc" element={<NFCPage />} />
+          <Route path="/nfc-card" element={<NFCCard />} />
+          <Route path="/profile" element={<Profile />} /> {/* Correct Profile Component */}
           <Route
-            path='/awareness'
-            element={<Awareness />}
-          />
-          <Route
-            path='/progress'
-            element={<Progress />}
-          />
-          <Route
-            path='/nfc'
-            element={<NFCPage />}
-          />
-          <Route
-            path='/profile'
-            element={<Progress />}
-          />
-          <Route
-            path='/'
+            path="/"
             element={
               <AuthComponent
                 districts={districts}
@@ -67,19 +53,16 @@ function LayoutContent({ data, credits }) {
         </Routes>
       </main>
     </div>
-  )
+  );
 }
 
 export default function Layout() {
-  const data = [70, 50, 90, 30, 100, 45, 80, 60, 25, 85, 55, 80]
-  const credits = 500
+  const data = [70, 50, 90, 30, 100, 45, 80, 60, 25, 85, 55, 80];
+  const credits = 500;
 
   return (
     <Router>
-      <LayoutContent
-        data={data}
-        credits={credits}
-      />
+      <LayoutContent data={data} credits={credits} />
     </Router>
-  )
+  );
 }
