2baba65d4c459855f7eeb264419fa98245d56681
Densingh <ddensingh19@gmail.com>
2024-10-24 16:36:19 +0530

    Navbar Responsiveness Started

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Navbar() {
-  const location = useLocation(); // Get the current location
+  const location = useLocation()
+  const [isOpen, setIsOpen] = useState(false)
   const links = [
     { href: '/dashboard', label: 'Dashboard' },
     { href: '/awareness', label: 'Awareness' },
@@ -9,22 +11,36 @@ export default function Navbar() {
     { href: '/nfc', label: 'NFC Pairing' },
     { href: '/profile', label: 'Profile' },
     { href: '/', label: 'Log out' },
-  ];
+  ]
 
   return (
-    <header className="fixed top-0 w-full bg-black z-10 shadow-lg">
-      <div className="container mx-auto px-4 py-2 flex items-center justify-between">
-        <Link to="/dashboard">
-          <img src="/1.png" className="w-12 h-10" alt="Logo" />
-        </Link>
-        <nav className="flex gap-4">
+    <header className='fixed top-0 w-full z-10 shadow-lg rounded-b-lg bg-medium-green'> {/* Apply rounded only at the bottom */}
+      <div className='mx-auto px-4 flex items-center justify-between'>
+        <div
+          style={{ backgroundImage: 'url(/1.png)' }}
+          className='w-28 aspect-video bg-cover bg-center'
+        ></div>
+        <button
+          className='lg:hidden md:hidden focus:outline-none bg-transparent w-12'
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <img src={isOpen ? './xmark-solid.svg' : './bars-solid.svg'} />
+        </button>
+        <nav
+          className={`flex-col absolute right-0 top-10 bg-medium-green shadow-lg ${
+            isOpen ? 'flex' : 'hidden'
+          } md:flex-row lg:flex-row md:flex lg:flex md:relative md:top-0 lg:relative lg:top-0 md:shadow-none lg:shadow-none`}
+        >
           {links.map(({ href, label }) => (
             <Link
               key={href}
               to={href}
-              className={`text-green-500 hover:text-green-600 transition-colors duration-300 ${
-                location.pathname === href ? 'font-semibold' : 'font-medium'
+              className={`hover:text-light-green transition-colors duration-100 px-4 py-2 ${
+                location.pathname === href
+                  ? 'text-light-green'
+                  : 'text-lightest-green'
               }`}
+              onClick={() => setIsOpen(false)}
             >
               {label}
             </Link>
@@ -32,5 +48,5 @@ export default function Navbar() {
         </nav>
       </div>
     </header>
-  );
+  )
 }
