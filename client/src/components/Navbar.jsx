07a104faa261054c0fa574f51152a5bf734010f4
Chokkalingam.s <chok22017.it@rmkec.ac.in>
2024-11-03 21:34:06 +0530

    Activity Page !

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,47 +1,45 @@
-import { Link, useLocation } from 'react-router-dom'
-import { useState } from 'react'
+import {Link, useLocation} from 'react-router-dom'
+import {useState} from 'react'
 
 export default function Navbar() {
   const location = useLocation()
   const [isOpen, setIsOpen] = useState(false)
   const links = [
-    { href: '/dashboard', label: 'Dashboard' },
-    { href: '/awareness', label: 'Awareness' },
-    { href: '/progress', label: 'Progress' },
-    { href: '/nfc', label: 'NFC Pairing' },
-    { href: '/profile', label: 'Profile' },
-    { href: '/', label: 'Log out' },
+    {href: '/dashboard', label: 'Dashboard'},
+    {href: '/awareness', label: 'Awareness'},
+    {href: '/progress', label: 'Progress'},
+    {href: '/nfc', label: 'NFC Pairing'},
+    {href: '/profile', label: 'Profile'},
+    {href: '/', label: 'Log out'}
   ]
-
   return (
-    <header className='fixed top-0 w-full z-10 shadow-lg rounded-b-lg bg-medium-green'> {/* Apply rounded only at the bottom */}
-      <div className='mx-auto px-4 flex items-center justify-between'>
+    <header className='fixed top-0 w-full shadow-lg rounded-ee-lg rounded-es-lg bg-medium-green h-12 z-20'>
+      <div className='flex items-center justify-between'>
         <div
-          style={{ backgroundImage: 'url(/1.png)' }}
-          className='w-28 aspect-video bg-cover bg-center'
-        ></div>
+          style={{backgroundImage: 'url(/1.png)'}}
+          className='w-24 h-12 bg-contain bg-no-repeat bg-center ml-2'></div>
         <button
           className='lg:hidden md:hidden focus:outline-none bg-transparent w-12'
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <img src={isOpen ? './xmark-solid.svg' : './bars-solid.svg'} />
+          onClick={() => setIsOpen(!isOpen)}>
+          <img
+            src={isOpen ? './xmark-solid.svg' : './bars-solid.svg'}
+            className='w-4/12'
+          />
         </button>
         <nav
-          className={`flex-col absolute right-0 top-10 bg-medium-green shadow-lg ${
+          className={`flex-col absolute right-0 top-12 z-20 rounded-lg ${
             isOpen ? 'flex' : 'hidden'
-          } md:flex-row lg:flex-row md:flex lg:flex md:relative md:top-0 lg:relative lg:top-0 md:shadow-none lg:shadow-none`}
-        >
-          {links.map(({ href, label }) => (
+          } md:flex-row lg:flex-row md:flex lg:flex md:relative md:top-0 lg:relative lg:top-0 bg-medium-green md:bg-transparent lg:bg-transparent`}>
+          {links.map(({href, label}) => (
             <Link
               key={href}
               to={href}
-              className={`hover:text-light-green transition-colors duration-100 px-4 py-2 ${
+              className={`hover:text-dark-green md:text-base lg:text-base transition-colors duration-200 px-4 py-2 ${
                 location.pathname === href
-                  ? 'text-light-green'
+                  ? 'text-dark-green text-lg'
                   : 'text-lightest-green'
               }`}
-              onClick={() => setIsOpen(false)}
-            >
+              onClick={() => setIsOpen(false)}>
               {label}
             </Link>
           ))}
