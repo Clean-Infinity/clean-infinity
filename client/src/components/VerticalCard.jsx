a28781b5206b087a3ee7c23c2c908b42389e51ba
Chokkalingam.s <chok22017.it@rmkec.ac.in>
2024-11-06 15:39:54 +0530

    Layout pages !

--- client/src/components/VerticalCard.jsx
+++ client/src/components/VerticalCard.jsx
@@ -1,18 +1,20 @@
-export default function VerticalCard({ titles }) {
+import {Link} from 'react-router-dom'
+
+export default function VerticalCard({titles}) {
+  console.log(titles)
   return (
-    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 items-center justify-center w-full max-w-4xl'>
-      {titles.map((title, i) => (
-        <div
-          className='p-2 flex flex-col items-center justify-center bg-light-green rounded-lg shadow-xl hover:shadow-md transition-shadow duration-300'
-          key={i}
-        >
-          <img
-            src='https://placehold.co/150'
-            className='w-full h-32 object-cover rounded-lg mb-4 shadow-lg'
-            alt={`${title} Image`}
-          />
-          <p className='font-medium text-lg text-center'>{title}</p>
-        </div>
+    <div className='grid grid-cols-3 md:grid-cols-6 lg:grid-cols-6 items-center gap-4 max-w-4xl'>
+      {titles.map(({title, path}) => (
+        <Link to={path} key={path}>
+          <div className='p-2 w-28 flex flex-col items-center justify-center drop rounded-lg shadow-lg hover:shadow-md transition-shadow'>
+            <img
+              src='https://placehold.co/50'
+              className='h-24 object-cover rounded-lg mb-2 shadow-md'
+              alt={`${title} Image`}
+            />
+            <p className='font-medium text-center'>{title}</p>
+          </div>
+        </Link>
       ))}
     </div>
   )
